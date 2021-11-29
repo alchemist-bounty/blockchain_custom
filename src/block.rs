c9e55eb34dcf13d21f1ac2c5b28f9b26b517be30
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-29 08:43:40 -0500

    Define Block and implementation

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,13 @@
 use std::fmt::{ self, Debug, Formatter };
+use super::*;
 
 pub struct Block {
+    pub index: u32,
+    pub timestamp: u32,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nonce: u64,
+    pub payload: String,
 }
 
 impl Debug for Block {
@@ -10,4 +17,15 @@ impl Debug for Block {
 }
 
 impl Block {
+    pub fn new( index: u32, timestamp: u32, hash: BlockHash, prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
+        Block {
+            index,
+            timestamp,
+            hash: vec![0; 32],
+            prev_block_hash,
+            nonce,
+            payload,
+        }
+    }
 }
+ 
