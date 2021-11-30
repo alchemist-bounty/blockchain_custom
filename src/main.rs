aaf2782c6ecd17cab7b6f20e61b12d829c85883f
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-29 23:32:10 -0500

    adjust nonce

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use blockchainlib::*;
 
 fn main () {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "Custom block".to_owned(), 0x00ffffffffffffffffffffffffffffffff );
+    let mut block = Block::new(0, 0, vec![0; 32], 118318, "Genesis block!".to_owned(), 0x0000ffffffffffffffffffffffffffff );
     block.hash = block.hash();
 
     println!("{:?}", &block);
 
-    block.mine();
+    // block.mine();
 
-    println!("{:?}", &block);
+    // println!("{:?}", &block);
 }
