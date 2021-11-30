f70f9b2fcae148e398502d4745cb66eb2158c88f
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-29 19:25:37 -0500

    hash block

--- src/block.rs
+++ src/block.rs
@@ -3,7 +3,7 @@ use super::*;
 
 pub struct Block {
     pub index: u32,
-    pub timestamp: u32,
+    pub timestamp: u128,
     pub hash: BlockHash,
     pub prev_block_hash: BlockHash,
     pub nonce: u64,
@@ -22,7 +22,7 @@ impl Debug for Block {
 }
 
 impl Block {
-    pub fn new( index: u32, timestamp: u32, prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
+    pub fn new( index: u32, timestamp: u128, prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
         Block {
             index,
             timestamp,
@@ -41,7 +41,7 @@ impl Hashable for Block {
         bytes.extend(&u128_bytes(&self.timestamp));
         bytes.extend(&self.prev_block_hash);
         bytes.extend(&u64_bytes(&self.nonce));
-        bytes.extend(&self.payload.as_bytes());
+        bytes.extend(self.payload.as_bytes());
         bytes
     }
 }
