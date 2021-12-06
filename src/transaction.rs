ab4ae227f355ced0541a0347f5ad2b5cbd291638
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-12-05 23:44:00 -0500

    implement hashable to transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,9 +1,40 @@
+use super::*;
+
 pub struct Output {
     pub to_addr: Address,
     pub value: u64,
 }
 
+impl Hashable for Output {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(self.to_addr.as_bytes());
+        bytes.extend(&u64_bytes(&self.value));
+    }
+}
+
 pub struct Transaction {
     pub inputs: Vec<Output>,
     pub outputs: Vec<Output>,
+}
+
+impl Hashable for Transaction {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        bytes.extend(
+            self.inputs
+                .iter()
+                .flat_map(|input| input.bytes())
+                .collect::<Vec<u8>>()
+        );
+
+        bytes.extend(
+            self.outputs
+                .iter()
+                .flat_map(|output| output.bytes())
+                .collect::<Vec<u8>>()
+        );
+    }
 }
